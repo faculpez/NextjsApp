3ed6e325934512c6648d629d39fa6b56de8ef613
faculpez <faculpez1998@gmail.com>
2023-08-29 21:01:41 -0300

    navbar-Links

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, fugit aut? Sapiente modi ipsam delectus eligendi officia in, deleniti incidunt voluptas nulla porro nemo perferendis corrupti facilis, sit nam dolorem?</p>
+    </>
+  )
+}
+
+export default about
