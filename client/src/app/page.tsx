2e37aec40147346bada2b154d134135223fa43ee
faculpez <faculpez1998@gmail.com>
2023-08-29 22:56:31 -0300

    getData

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { userData } from '@/components/userData'
 import React from 'react'
 
 const Home = () => {
+  const users = userData
+  console.log(users)
   return (
     <div>
       Hello
